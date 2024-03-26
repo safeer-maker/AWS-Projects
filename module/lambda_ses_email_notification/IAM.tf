bdcd6f106fccad4bcaeda3721498d8c704d2cdbd
Safeer Linux <bsee15055@itu.edu.pk>
2024-03-27 02:55:41 +0500

    IAM role for lambda

--- module/lambda_ses_email_notification/IAM.tf
+++ module/lambda_ses_email_notification/IAM.tf
@@ -5,3 +5,9 @@ resource "aws_iam_policy" "lambda_ses_policy" {
     description = "Allow lambda to send email using SES"
     policy = file("${path.module}/policy_json/lambda_ses_policy.json")
 }
+
+resource "aws_iam_role" "lambda_ses_role" {
+    name = "lambda_ses_role"
+    assume_role_policy = file("${path.module}/policy_json/lambda_assume_role_policy.json")
+    managed_policy_arns = [aws_iam_policy.lambda_ses_policy.arn]
+}

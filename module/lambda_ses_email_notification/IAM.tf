69fa9bb48033078cca24db491c7bf5b977b16cc5
Safeer Linux <bsee15055@itu.edu.pk>
2024-04-02 04:34:16 +0500

    state machine IAM role

--- module/lambda_ses_email_notification/IAM.tf
+++ module/lambda_ses_email_notification/IAM.tf
@@ -11,3 +11,17 @@ resource "aws_iam_role" "lambda_ses_role" {
     assume_role_policy = file("${path.module}/policy_json/lambda_assume_role_policy.json")
     managed_policy_arns = [aws_iam_policy.lambda_ses_policy.arn]
 }
+
+# Attach SES policy to the role
+
+resource "aws_iam_policy" "state_machine_policy" {
+    name = "sm_policy"
+    description = "Allow State Machine to create cloud watch logs and invoke lambda function"
+    policy = file("${path.module}/policy_json/state_machine_policy.json")
+}
+
+resource "aws_iam_role" "state_machine_role" {
+    name = "sm_role"
+    assume_role_policy = file("${path.module}/policy_json/state_machine_assume_role_policy.json")
+    managed_policy_arns = [aws_iam_policy.state_machine_policy.arn]
+}

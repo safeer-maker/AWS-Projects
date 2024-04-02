705a1acc43183c6882c17b7fa72534f3db17d415
Safeer Linux <bsee15055@itu.edu.pk>
2024-04-03 01:25:06 +0500

    Lambda for api is added

--- module/lambda_ses_email_notification/Lambda.tf
+++ module/lambda_ses_email_notification/Lambda.tf
@@ -1,6 +1,11 @@
+locals {
+  lambda_ses_file_name = "lambda_ses"
+  lambda_api_file_name = "lambda_api"
+}
+
 resource "aws_lambda_function" "lambda_ses" {
 
-  filename      = "${path.module}/assets/lambda_ses.zip"
+  filename      = "${path.module}/assets/${local.lambda_ses_file_name}.zip"
   function_name = "lambda_ses_sms"
   role          = aws_iam_role.lambda_ses_role.arn
   handler       = "index.test"
@@ -15,4 +20,38 @@ resource "aws_lambda_function" "lambda_ses" {
       FROM_EMAIL_ADDRESS = var.email_primary
     }
   }
+  depends_on = [ terraform_data.zipfile ]
 }
+
+resource terraform_data "zipfile" {
+  provisioner "local-exec" {
+    command = "zip -r ${path.module}/assets/${local.lambda_ses_file_name}.zip ${path.module}/assets/${local.lambda_ses_file_name}.py" 
+  }
+}
+
+resource terraform_data "zipfile_api" {
+  provisioner "local-exec" {
+    command =  "zip -r ${path.module}/assets/${local.lambda_api_file_name}.zip ${path.module}/assets/${local.lambda_api_file_name}.py"
+            
+  }
+}
+
+resource "aws_lambda_function" "lambda_api" {
+  filename      = "${path.module}/assets/${local.lambda_api_file_name}.zip"
+  function_name = "lambda_api"
+  role          = aws_iam_role.lambda_ses_role.arn
+  handler       = "index.test"
+  architectures = ["x86_64"]
+
+  timeout = "60"
+  memory_size = 128
+  runtime = "python3.12"
+
+  environment {
+    variables = {
+      FROM_EMAIL_ADDRESS = var.email_primary
+    }
+  }
+  depends_on = [ terraform_data.zipfile_api ]
+}
+

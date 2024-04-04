14d7f8081ab801ea6193af4b74b4557cb4adf870
Safeer Linux <bsee15055@itu.edu.pk>
2024-04-04 23:42:30 +0500

    S3 bucket is added

--- module/lambda_ses_email_notification/s3.tf
+++ module/lambda_ses_email_notification/s3.tf
@@ -1,6 +1,6 @@
 locals {
     serverless_file = "serverless.js"
-    bucket_name = "web_bucket"
+    web_bucket_name = "web-bucket-143"
 }
 
 # data "backed" "envars" {
@@ -8,8 +8,7 @@ locals {
 # }
 
 resource "aws_s3_bucket" "web_bucket" {
-    bucket = var.web_bucket_name
-
+    bucket = local.web_bucket_name
 
     force_destroy = true
 }
@@ -17,10 +16,10 @@ resource "aws_s3_bucket" "web_bucket" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.web_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "example" {
@@ -31,26 +30,21 @@ resource "aws_s3_bucket_website_configuration" "example" {
   }
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.web_bucket.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-}
-
-data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["123456789012"]
+resource "aws_s3_bucket_policy" "b" {
+   bucket = aws_s3_bucket.web_bucket.id
+   policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["${aws_s3_bucket.web_bucket.arn}/*"]
     }
+  ]
+}
+POLICY
 
-    actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
-    ]
-
-    resources = [
-      aws_s3_bucket.web_bucket.arn,
-      "${aws_s3_bucket.web_bucket.arn}/*",
-    ]
-  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
-##############################################
-# lambda_s3_image_compression outputs
+# ##############################################
+# # lambda_s3_image_compression outputs
 
-# output "s3_bucket_names" {
-#     value = [
-#             module.lambda_s3_compression.source_bucket,
-#             module.lambda_s3_compression.processed_bucket
-#             ]
-# }
+# # output "s3_bucket_names" {
+# #     value = [
+# #             module.lambda_s3_compression.source_bucket,
+# #             module.lambda_s3_compression.processed_bucket
+# #             ]
+# # }
 
-# output "name" {
-#     value = module.lambda_s3_compression.user_id
+# # output "name" {
+# #     value = module.lambda_s3_compression.user_id
   
-# }
+# # }
 
 
-##############################################
-# lambda_ses_email_notification outputs
+# ##############################################
+# # lambda_ses_email_notification outputs
 
 output "lambda_arn" {
     value = module.email_notification.lambda_arn

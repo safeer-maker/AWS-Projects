647fcb01f4c958e1d23c49ad285160eda73c0a55
Safeer Linux <bsee15055@itu.edu.pk>
2024-04-06 00:41:29 +0500

    S3 all file uploaded and modefied with envar variables

--- module/lambda_ses_email_notification/s3.tf
+++ module/lambda_ses_email_notification/s3.tf
@@ -1,11 +1,8 @@
 locals {
     serverless_file = "serverless.js"
     web_bucket_name = "web-bucket-143"
+    file_name = "serverless.js"
 }
-
-# data "backed" "envars" {
-#     file = templatefile("${path.module}/assets/${local.serverless_file}" , { API_GATEWAY_ARN = aws_api_gateway_rest_api.lambda_api.arn })
-# }
 
 resource "aws_s3_bucket" "web_bucket" {
     bucket = local.web_bucket_name
@@ -48,3 +45,36 @@ resource "aws_s3_bucket_policy" "b" {
 POLICY
 
 }
+
+resource "aws_s3_object" "web_files" {
+  for_each = fileset("${path.module}/assets/serverless_frontend", "*.*")
+
+  bucket = aws_s3_bucket.web_bucket.id
+  key = each.key
+  source = "${path.module}/assets/serverless_frontend/${each.key}"
+  etag = filemd5("${path.module}/assets/serverless_frontend/${each.key}")  
+}
+
+resource "template_file" "add_arn" {
+  template = file ("${path.module}/assets/serverless_frontend/${local.serverless_file}")
+
+  vars = { API_GATEWAY_ARN = aws_api_gateway_rest_api.lambda_api.arn }
+  depends_on = [ aws_s3_object.web_files ]
+}
+
+resource "local_file" "temp_data_file" {
+  filename = "${path.module}/temp_data.txt"
+  content  = template_file.add_arn.rendered
+
+  depends_on = [ template_file.add_arn ]
+}
+
+resource "aws_s3_object" "file_mod" {
+  bucket = aws_s3_bucket.web_bucket.id
+  key = local.serverless_file
+  source = local_file.temp_data_file.filename
+  etag = filemd5("${path.module}/assets/serverless_frontend/${local.serverless_file}")
+
+  depends_on = [ template_file.add_arn]
+}
+
